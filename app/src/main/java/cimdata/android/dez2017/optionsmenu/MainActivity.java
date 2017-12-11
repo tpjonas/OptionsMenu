35e02a8a86f9cad49ad6b473528cea8913998d72
Jonas <tpjonas@gmail.com>
2017-12-11 08:51:36 +0100

    monday morning change

--- app/src/main/java/cimdata/android/dez2017/optionsmenu/MainActivity.java
+++ app/src/main/java/cimdata/android/dez2017/optionsmenu/MainActivity.java
@@ -40,7 +40,7 @@ public class MainActivity extends AppCompatActivity {
             case R.id.menu_main_item_01:
                 Toast.makeText(
                         this,            // Kontext
-                        "Item fuktioniert immer noch foobar barfoo new commit foolio exit",
+                        "Item fuktioniert immer noch foobar barfoo new commit foolio exit coolio",
                         Toast.LENGTH_LONG       // Dauer der Anzeige
                 ).show();
                 break;
